07869eaff122ded4ec1151bb79daac628670dca4
Minhaz Uddin Emon <45569539+minhazemon21@users.noreply.github.com>
2022-06-30 18:20:29 +0600

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EKapp.Models;
+
+namespace EKapp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shawshank Redumption!",
+                Id = 17
+            };
+            return View(movie);
+            //return Content("Hi MSK");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+
+
+        }
+       
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EKapp.MvcApplication" Language="C#" %>
