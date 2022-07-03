923afa738f15055dc5c286d9bcc40fe9733e0589
Minhaz Uddin Emon <45569539+minhazemon21@users.noreply.github.com>
2022-07-03 10:51:19 +0600

    gh

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,5 +10,6 @@ namespace EKapp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string AuthorName { get; set; }
+        public string Actress { get; set; }
     }
 }
